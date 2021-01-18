226194dcc8ce63e4bcc5e61d7fd9d3a9c6f0074c
Enrique De Lavalle <enrique_delavalle@epam.com>
2021-01-18 19:40:49 +0100

    first commit

---
+++ src/main/java/com/epam/calculatorLambda/CalculatorLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.epam.calculatorLambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/calculatorLambda/CalculatorLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.calculatorLambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
