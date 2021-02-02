c7132f7ffa96d9ed56b2d78dbe6745b562a2206f
Lukasz_Galeziowski <lukasz_galeziowski@epam.com>
2021-02-02 11:16:31 +0100

    Simplify switch, refactor usage of lambdas

--- src/main/java/com/epam/calculatorLambda/services/Calculator.java
+++ src/main/java/com/epam/calculatorLambda/services/Calculator.java
@@ -1,6 +1,8 @@
 package com.epam.calculatorLambda.services;
 
+import java.util.function.BinaryOperator;
+
 @FunctionalInterface
 public interface Calculator {
-	public Double calculate(Double num1, Double num2);
+	public Double calculate(Double num1, Double num2, BinaryOperator<Double> operator);
 }
