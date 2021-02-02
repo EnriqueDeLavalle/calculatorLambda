f1b85009f2bb90a3d67bce0c2f4619b4c7584284
Lukasz_Galeziowski <lukasz_galeziowski@epam.com>
2021-02-02 11:30:20 +0100

    Removed unused class

--- src/main/java/com/epam/calculatorLambda/services/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.epam.calculatorLambda.services;
-
-import java.util.function.BinaryOperator;
-
-@FunctionalInterface
-public interface Calculator {
-	public Double calculate(Double num1, Double num2, BinaryOperator<Double> operator);
-}
